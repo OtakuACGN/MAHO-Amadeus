6bb7ccdcc38b3a68399b2f8c2461d6b43bf6e154
bysq <1904568840@qq.com>
2025-11-30 22:24:36 +0800

    找好素材了

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import './assets/reset.css'
+</script>
 
 <template>
+  <router-view />
 </template>
 
 <style scoped></style>
---
+++ frontend/src/pages/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home-page">
+
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.home-page {
+  width: 100vw;
+  height: 100vh;
+  background: url('/bg.png') center center no-repeat;
+  background-size: cover;
+}
+</style>
