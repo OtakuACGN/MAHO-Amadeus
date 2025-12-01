463ba461711be97aeacb163deadb8b60fe6b719e
bysq <1904568840@qq.com>
2025-12-01 10:07:17 +0800

    new

---
+++ frontend/src/component/SpritePlayer.vue
@@ -0,0 +1,62 @@
+<template>
+  <div
+    :style="{
+      width: width + 'px',
+      height: height + 'px',
+      overflow: 'hidden',
+      position: 'relative',
+      display: 'inline-block'
+    }"
+  >
+    <img
+      :src="src"
+      :style="{
+        position: 'absolute',
+        left: -frameX * width + 'px',
+        top: -frameY * height + 'px',
+        width: columns * width + 'px',
+        height: rows * height + 'px',
+        imageRendering: 'pixelated'
+      }"
+      draggable="false"
+      alt="sprite"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref, computed, onMounted, onUnmounted, watch } from 'vue'
+
+const props = defineProps({
+  src: { type: String, required: true },      // 图片路径
+  rows: { type: Number, required: true },     // 行数
+  columns: { type: Number, required: true },  // 列数
+  fps: { type: Number, default: 12 },         // 帧率
+  width: { type: Number, required: true },    // 单帧宽度
+  height: { type: Number, required: true },   // 单帧高度
+  totalFrames: { type: Number, required: true } // 总帧数
+})
+
+const frame = ref(0)
+let timer = null
+
+const actualTotalFrames = computed(() =>
+  props.totalFrames ?? (props.rows * props.columns)
+)
+
+const frameX = computed(() => frame.value % props.columns)
+const frameY = computed(() => Math.floor(frame.value / props.columns))
+
+function play() {
+  timer = setInterval(() => {
+    frame.value = (frame.value + 1) % actualTotalFrames.value
+  }, 1000 / props.fps)
+}
+
+onMounted(play)
+onUnmounted(() => timer && clearInterval(timer))
+watch(() => props.fps, (newFps) => {
+  if (timer) clearInterval(timer)
+  play()
+})
+</script>
--- frontend/src/pages/home.vue
+++ frontend/src/pages/home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home-page">
-
+    <SpritePlayer src="/sprite_logo.png" :rows="6" :columns="7" :fps="20" :width="200" :height="200" />
   </div>
 </template>
 
 <script setup>
+import SpritePlayer from '@/component/SpritePlayer.vue';
 </script>
 
 <style scoped>
 .home-page {
-  width: 100vw;
-  height: 100vh;
-  background: url('/bg.png') center center no-repeat;
+  background-image: url('/bg.png');
   background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  min-height: 100vh;
 }
 </style>
